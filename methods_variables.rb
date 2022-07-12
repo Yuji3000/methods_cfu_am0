a09e51c869e33f12252331fc0e8e9dceb757d588
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-07-12 12:52:15 -0500

    Added answers

--- methods_variables.rb
+++ methods_variables.rb
@@ -8,7 +8,15 @@
 #  call reverse on the variable, print it out
 #  call length on the variable, print it out
 
+Classmate_names = "Billy"
 
+puts Classmate_names.upcase
+
+puts Classmate_names.downcase
+
+puts Classmate_names.reverse
+
+puts Classmate_names.length
 
 
 
@@ -16,17 +24,25 @@
 
 # =================================
 # PART 2
+last_login = "  12/09/2021  "
 
-user_name = "coco_11am"
-last_login = "12/09/2021"
+user_name = "coco_11am  "
 
-# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
-  # Write the methods out on the lines below, with an explanation in your own words of how they work.
+puts user_name.capitalize
+#The method capitalize will make the first letter of the string a capital letter
 
+puts last_login.lstrip
+#The method lstrip will remove any whitespace/open spaces at the beginning of the string IF there are any.
+#For lstrip, if no whitespace is found, it will return the original String
+puts user_name.reverse!
+#Reverse! is a method that will reverse the string characters in a permanant way.
+#If the same string is called again, it will show the reversed version as the new string input
+puts last_login.rstrip
+#Similar to lstrip, but rstrip with remove the trailing whitespace within a string
 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
 
-
-
-
+#The '!' symbol with modify the array/variable rather than outputing a modification of the original
+#Shown above .reverse will reverse a string and print the output.
+# While .reverse! will modify the copy
