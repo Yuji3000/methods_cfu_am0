0d868a04fa047dad43a43e4689eb133bbb0b3ab5
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2022-07-12 12:08:24 -0500

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,29 @@
 
 
 "Hello World".include?("Hello")
+# The include? method is called on the string object "Hello World"
+#The "Hello" argument is passed to check if the string "Hello World" has the argument included within it "
+# The return value is True
 
+end
 
 "Hello World".end_with?("Hello")
+# The end_with? method is called on the string object "Hellow World"
+# The argument .end_with? is passed. end_with? identifies if the string "Hello World" has "Hello" in the end of that string.
+# The string does not end with "Hello" and the output is false.
 
 
 "Hello World".end_with?("rld")
-
+# The argument "rld" is passed on the string "Hello World"
+# The return value is true.
 
 12.even?
+# The even? method is called for the integer 12.
+# Even? method checks if 12 is an even number
+# The output is true.
 
 
 18.next
-
-
+# The next method is called on the integer 18
+# next method is used to find out what is after 18
+# The output is 19
